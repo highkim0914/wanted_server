6e6b09c18a5955488a388852f969ba8934b52437
highkim0922 <shineest96@gmail.com>
2022-06-26 20:40:41 +0900

    modify : entity 속성 추가

--- src/main/java/com/risingtest/wanted/src/user/model/User.java
+++ src/main/java/com/risingtest/wanted/src/user/model/User.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
     private int id;
     private String userName;
     private String password;

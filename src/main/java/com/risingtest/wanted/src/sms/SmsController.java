0ea1678c556941d061eb9756b9993ce43c2daeb3
highkim0922 <shineest96@gmail.com>
2022-06-26 20:42:39 +0900

    add : 전화번호 인증 토큰을 위한 jwt Service 추가

--- src/main/java/com/risingtest/wanted/src/sms/SmsController.java
+++ src/main/java/com/risingtest/wanted/src/sms/SmsController.java
@@ -3,6 +3,7 @@ package com.risingtest.wanted.src.sms;
 import com.risingtest.wanted.config.BaseException;
 import com.risingtest.wanted.config.BaseResponse;
 import com.risingtest.wanted.config.BaseResponseStatus;
+import com.risingtest.wanted.utils.JwtService;
 import com.risingtest.wanted.utils.ValidationRegex;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,13 @@ public class SmsController {
 
     private final SmsService smsService;
 
-    @Autowired
-    public SmsController(SmsService smsService) {
-        this.smsService = smsService;
-    }
+    private final JwtService jwtService;
 
+    @Autowired
+    public SmsController(SmsService smsService, JwtService jwtService) {
+        this.smsService = smsService;
+        this.jwtService = jwtService;
+    }
     @PostMapping
     public BaseResponse<String> createAuthenticationCode(@RequestBody PostSmsReq postSmsReq){
         if(!ValidationRegex.isRegexPhoneNumber(postSmsReq.getPhoneNumber())){
@@ -34,14 +37,16 @@ public class SmsController {
     }
 
     @PostMapping("/authentication")
-    public BaseResponse<String> authenticateCode(@RequestBody PostSmsAuthenticationReq smsAuthenticationDto){
+    public BaseResponse<PostSmsAuthenticationRes> authenticateCode(@RequestBody PostSmsAuthenticationReq smsAuthenticationDto){
         if(!ValidationRegex.isRegexPhoneNumber(smsAuthenticationDto.getPhoneNumber())){
             return new BaseResponse<>(BaseResponseStatus.SMS_INVALID_PHONE_NUMBER);
         }
         String phoneNumber = smsAuthenticationDto.getPhoneNumber().replace("-","");
         String code = smsAuthenticationDto.getCode();
-        if(smsService.isValidCode(phoneNumber, code))
-            return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+        if(smsService.isValidCode(phoneNumber, code)) {
+            String jwt = jwtService.createJwtUsingPhoneNumber(phoneNumber);
+            return new BaseResponse<>(new PostSmsAuthenticationRes(jwt));
+        }
         else
             return new BaseResponse<>(BaseResponseStatus.SMS_WRONG_CODE);
 

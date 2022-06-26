b2ab814a88619ec0bfdab00310e98d86e7f94946
highkim0922 <shineest96@gmail.com>
2022-06-26 20:42:59 +0900

    add : PostSmsAuthenticationRes 추가

---
+++ src/main/java/com/risingtest/wanted/src/sms/PostSmsAuthenticationRes.java
@@ -0,0 +1,13 @@
+package com.risingtest.wanted.src.sms;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSmsAuthenticationRes {
+    private String jwt;
+}

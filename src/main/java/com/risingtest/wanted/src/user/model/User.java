514564f1835461192cbdd370d6f418c62a6ffd7b
highkim0922 <shineest96@gmail.com>
2022-06-26 21:37:40 +0900

    modify : entity 어노테이션 수정

--- src/main/java/com/risingtest/wanted/src/user/model/User.java
+++ src/main/java/com/risingtest/wanted/src/user/model/User.java
@@ -1,16 +1,16 @@
 package com.risingtest.wanted.src.user.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//@EqualsAndHashCode(callSuper = true)
 @Entity
 public class User {
     @Id

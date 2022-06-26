f0924e236c58b613393bc190ff6ff745e9cfeae3
highkim0922 <shineest96@gmail.com>
2022-06-26 19:46:11 +0900

    add : SMS_INVALID_PHONE_NUMBER 추가

--- src/main/java/com/risingtest/wanted/config/BaseResponseStatus.java
+++ src/main/java/com/risingtest/wanted/config/BaseResponseStatus.java
@@ -22,6 +22,8 @@ public enum BaseResponseStatus {
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
 
+    SMS_INVALID_PHONE_NUMBER(false, 2005, "전화번호 형식을 확인해주세요"),
+
     // users
     USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
 
@@ -51,11 +53,16 @@ public enum BaseResponseStatus {
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
     SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
 
+    SMS_ERROR(false,4005,"SMS 인증 서비스 오류입니다."),
+    SMS_WRONG_CODE(false,4006,"유효하지 않은 인증 코드입니다."),
+
+
     //[PATCH] /users/{userIdx}
     MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
 
     PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
     PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+
 
 
     // 5000 : 필요시 만들어서 쓰세요

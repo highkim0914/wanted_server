14986c0ac57e0b4aa01123b7b91b25108f974bce
highkim0922 <shineest96@gmail.com>
2022-07-27 16:24:17 +0900

    modify : NoArgsConstructor 추가

--- src/main/java/com/risingtest/wanted/src/user/model/PatchUserReq.java
+++ src/main/java/com/risingtest/wanted/src/user/model/PatchUserReq.java
@@ -2,11 +2,13 @@ package com.risingtest.wanted.src.user.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PatchUserReq {
     private int userIdx;
     private String userName;

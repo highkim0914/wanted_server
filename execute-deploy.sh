d027838a6a7502a61115c7bf3b0a22841532dd10
highkim0922 <shineest96@gmail.com>
2022-08-01 00:34:27 +0900

    modify : nohup 삭제

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /home/ubuntu/wanted/github-action/deploy.sh & > /home/ubuntu/wanted/execute-log.txt
+/home/ubuntu/wanted/github-action/deploy.sh & > /home/ubuntu/wanted/execute-log.txt

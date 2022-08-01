257dce715fdb3119487b129f45fd13be38e7503a
highkim0922 <shineest96@gmail.com>
2022-08-01 16:45:03 +0900

    modify : stdout 과 stderr 경로 추가 및 & 삭제

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/ubuntu/wanted/github-action/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ubuntu/wanted/github-action/deploy.sh > /home/ubuntu/wanted/log.out 2> /home/ubuntu/wanted/error.out < /dev/null
